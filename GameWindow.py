880963e0ec6bfd17153db9c81f8c5c1e3066def5
Egor Lanschekov <egorrkaboxer@gmail.com>
2023-12-27 00:04:03 +0300

    Initial commit 2.0
    - Add the necessary style resources.
    - Restructure the code.
    - Add a dinosaur movement effect without animation.

---
+++ GameWindow.py
@@ -0,0 +1,81 @@
+from typing import Any
+
+import pygame
+from system import SIZE, WIDTH, HEIGHT, FPS
+from system import load_image
+import random
+
+# load_image() requires pygame and screen initialization
+pygame.init()
+screen = pygame.display.set_mode(SIZE)
+
+
+class Tile(pygame.sprite.Sprite):
+    images = [load_image('tile_1.png'), load_image('tile_2.png')]
+    dx = -10
+
+    def __init__(self, x: int, *groups):
+        super(Tile, self).__init__(*groups)
+        self.image = random.choice(self.images)
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = x, 467
+
+    def update(self, *args: Any, **kwargs: Any) -> None:
+        self.rect = self.rect.move(self.dx, 0)
+
+        # If there is an empty space on the right that needs to be filled with tiles
+        if self.rect.x < 0 and len(tile_group) == 2:
+            Tile(WIDTH + self.rect.x, tile_group, all_sprites)
+
+        # If the tile is no longer visible
+        if self.rect.right < 0:
+            self.kill()
+
+
+class Dino(pygame.sprite.Sprite):
+    image = load_image('dino.png')
+
+    def __init__(self, *groups):
+        super(Dino, self).__init__(*groups)
+        self.image = Dino.image
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = 100, 367
+
+    def update(self, *args: Any, **kwargs: Any) -> None:
+        pass
+
+
+if __name__ == '__main__':
+    bg_image = load_image('game_bg.png')
+    clock = pygame.time.Clock()
+
+    # Sprite groups
+    all_sprites = pygame.sprite.Group()
+    dino_group = pygame.sprite.Group()
+    tile_group = pygame.sprite.Group()
+    cactus_group = pygame.sprite.Group()
+
+    # Initial tiles
+    Tile(0, tile_group, all_sprites)
+    Tile(WIDTH // 2, tile_group, all_sprites)
+
+    dino = Dino(dino_group, all_sprites)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.blit(bg_image, (0, 0))
+
+        tile_group.update()
+        tile_group.draw(screen)
+
+        dino_group.update()
+        dino_group.draw(screen)
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
+    pygame.quit()
---
+++ system.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+import pygame
+
+SIZE = WIDTH, HEIGHT = 1000, 650
+FPS = 30
+
+
+def load_image(name, colorkey=None):
+    fullname = 'data/' + name
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+
